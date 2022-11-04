927d1ca2a33124202b0933b0726b37378c461303
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2022-11-04 13:45:18 -0700

    mobile2

--- components/Web3Modal/connectors.tsx
+++ components/Web3Modal/connectors.tsx
@@ -2,23 +2,25 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
 
-const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+const injectedConnector = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42, 11155111],
 });
-
 const walletconnect = new WalletConnectConnector({
-  rpc: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  supportedChainIds: [1, 3, 4, 5, 42, 11155111],
+  rpc: { 1: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`, 5: `https://goerli.infura.io/v3/e0171a3aab904c6bbe6622e6598770ad` },
   bridge: "https://bridge.walletconnect.org",
-  qrcode: false
+  qrcode: true,  
 });
 
-const walletlink = new WalletLinkConnector({
+const CoinbaseWallet = new WalletLinkConnector({
   url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-  appName: "web3-react-demo"
+  appName: "Web3",
+  supportedChainIds: [1, 3, 4, 5, 42, 11155111],
 });
+
 
 export const connectors = {
-  injected: injected,
+  injected: injectedConnector,
   walletConnect: walletconnect,
-  coinbaseWallet: walletlink
+  coinbaseWallet: CoinbaseWallet,
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,12 @@ import type { AppProps } from 'next/app'
 import { Web3ReactProvider } from '@web3-react/core'
 
 import { Web3Provider } from '@ethersproject/providers'
+import { useEffect } from 'react'
+import {connectors } from '../components/Web3Modal/connectors'
+import { useWeb3React } from '@web3-react/core'
 
+const { chainId, account, activate, active, library, deactivate } =
+useWeb3React<Web3Provider>();
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider)
   library.pollingInterval = 12000
@@ -11,7 +16,10 @@ function getLibrary(provider: any): Web3Provider {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-
+  useEffect(() => {
+    const provider = window.localStorage.getItem("provider");
+    if (provider) activate(connectors as any[typeof provider]);
+  }, []);
   return(
   <Web3ReactProvider getLibrary={getLibrary}>
    <Component {...pageProps} />
