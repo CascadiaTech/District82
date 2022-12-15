f86a5a5178e776c3f3e5ae067abbcc0cebd2808b
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2022-12-15 07:37:24 -0800

    soundsfx

--- components/Dapp/dappsection.tsx
+++ components/Dapp/dappsection.tsx
@@ -1,6 +1,6 @@
 import { useWeb3React } from "@web3-react/core";
 import "../../styles/Home.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ConnectWallet } from "../Web3Modal/WalletConnect";
 import District from "../../assets/District.png";
 import Image from "next/image";
@@ -29,16 +29,13 @@ export default function DappComponent(props: any) {
   const [distict, setdistrictactive] = useState(false);
   const [buttonhidden, setbuttonhidden] = useState(true);
   const [loading, setLoading] = useState(false);
+  const [paused, setpaused] = useState(false);
   const [pendingreflections, setpendingreflections] = useState(Number);
-  const [uniswaprovider, setuniswapprivder] = useState();
-  //const D82contract = "0x00000000000000000000000000000";
 
+  const audioRef: any = useRef(null);
   useEffect(() => {
     async function Fetchbalance() {
       if (!account) {
-        console.log({
-          message: "Hold On there Partner, there seems to be an Account err!",
-        });
         return;
       }
 
@@ -111,7 +108,6 @@ export default function DappComponent(props: any) {
         );
         const finalnumber = Number(Reflections);
         settotaldistributed(finalnumber);
-        console.log(Reflections);
 
         return finalnumber;
       } catch (error) {
@@ -196,13 +192,40 @@ export default function DappComponent(props: any) {
   const Decimal_balance = insertDecimal(balance / 1000000000000);
   const formatted_balance = numberWithCommas(Decimal_balance);
 
-  const jsonRpcUrlMap = {
-    1: ["https://mainnet.infura.io/v3/fc5d70bd4f49467289b3babe3d8edd97"],
-    3: ["https://ropsten.infura.io/v3/<YOUR_INFURA_PROJECT_ID>"],
-  };
 
+  useEffect(() => {
+    audioRef.current.defaultMuted = false;
+  });
+  const playAudio = () => {
+    //setAudio(new Audio('./AIRGLOW.mp3')) // only call client
+    audioRef && audioRef.current;
+    if (audioRef.current.currentTime == 0) {
+      console.log(audioRef.currentTime);
+      audioRef.current.load();
+      audioRef.current.play().catch((error: any) => {
+        console.log("error attempting to play", error);
+      });
+      audioRef?.volume && audioRef?.volume == 0.5;
+      console.log("tried");
+    } else {
+      console.log("alreadyplaying");
+    }
+  };
+  const attemptPlay = () => {
+    audioRef.current.play();
+    audioRef?.volume && audioRef?.volume == 0.5;
+    audioRef.current.volume = 0.5
+    setpaused(false);
+  };
+  const attemptPause = () => {
+    audioRef.current.pause();
+    setpaused(true);
+  };
   return (
     <>
+      <audio ref={audioRef} id="player" playsInline autoPlay loop>
+        <source src="./AIRGLOW.mp3" type="audio/mp3" />
+      </audio>
       {buttonhidden || account ? (
         <>
           {distict ? (
@@ -364,8 +387,8 @@ export default function DappComponent(props: any) {
                   <Image
                     className="text-gray-500 elevation-24 hover:text-gray-900 hover:scale-110 transition-all duration-700 dark:hover:text-white"
                     onClick={() => setdistrictactive(true)}
-                    height={100}
-                    width={100}
+                    height={150}
+                    width={150}
                     src={District}
                   ></Image>
                   <p style={{ fontFamily: "Audiowide" }}> District82</p>
@@ -414,18 +437,18 @@ export default function DappComponent(props: any) {
                     "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
                   }
                 ></div>
-   <div className={"w-20 h-20"}>
+                <div className={"w-20 h-20"}>
                   {" "}
                   <Image
                     className="cursor-pointer text-gray-500 hover:scale-110 transition-all duration-700 dark:hover:text-white"
                     onClick={() =>
-                      window.open("https://twitter.com/TheDistrict82","_self")
+                      window.open("https://twitter.com/TheDistrict82", "_self")
                     }
                     height={100}
                     width={100}
                     src={twitter}
                   ></Image>
-                  <p style={{ fontFamily: "Audiowide" }}> Social net</p>
+                  <p style={{ fontFamily: "Audiowide"}}> Social net</p>
                 </div>
                 <div
                   className={
@@ -464,11 +487,13 @@ export default function DappComponent(props: any) {
                     "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
                   }
                 ></div>
-            <div className={"w-20 h-20"}>
+                <div className={"w-20 h-20"}>
                   {" "}
                   <Image
                     className="cursor-pointer text-gray-500 hover:scale-110 transition-all duration-700 dark:hover:text-white"
-                    onClick={() => window.open("https://t.me/Collective333","_self")}
+                    onClick={() =>
+                      window.open("https://t.me/Collective333", "_self")
+                    }
                     height={100}
                     width={100}
                     src={chat}
@@ -520,7 +545,8 @@ export default function DappComponent(props: any) {
                     className="cursor-pointer text-gray-500 hover:scale-110 transition-all duration-700 dark:hover:text-white"
                     onClick={() =>
                       window.open(
-                        "https://dextools.io/app/en/ether/pair-explorer/0xc4b478a43b357f9e76c7d6dc27eef8d78980eb5d","_self"
+                        "https://dextools.io/app/en/ether/pair-explorer/0xc4b478a43b357f9e76c7d6dc27eef8d78980eb5d",
+                        "_self"
                       )
                     }
                     height={100}
@@ -529,13 +555,102 @@ export default function DappComponent(props: any) {
                   ></Image>
                   <p style={{ fontFamily: "Audiowide" }}> Chart </p>
                 </div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "bg-transparent w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                ></div>
+                <div
+                  className={
+                    "cursor-pointer bg-transparent w-12 h-12 content-center items-center text-center  lg:w-20 lg:h-20 2xl:w-28 2xl:h-28"
+                  }
+                >
+                  {paused ? (
+                    <svg
+                      onClick={() => attemptPlay()}
+                      className="w-20 h-20 hover:scale-110 transition-all duration-500"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      onClick={() => attemptPause()}
+                      className="w-20 h-20 hover:scale-110 transition-all duration-500"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
+                        clipRule="evenodd"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"
+                      />
+                    </svg>
+                  )}
+                </div>
               </div>
             </div>
           )}
         </>
       ) : (
         <>
-          <div className="transition-all duration-1000 absolute flex flex-col mr-20 justify-center content-center items-center">
+          <div
+            onClick={() => playAudio()}
+            className="transition-all duration-1000 absolute flex flex-col mr-20 justify-center content-center items-center"
+          >
             <ConnectWallet></ConnectWallet>
           </div>
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,60 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import HeaderComponent from "../components/Header/HeaderComponent";
 import "tailwindcss-elevation";
-import FooterComponent from "../components/Footer/FooterComponent";
-import DualCardComponent from "../components/DualCards/DualCardComponent";
-import ScrollpositionAnimation from "../hooks/OnScroll";
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "@uniswap/widgets/fonts.css";
 import { useWeb3React } from "@web3-react/core";
+
 import DappComponent from "../components/Dapp/dappsection";
 const Home: NextPage = () => {
   const { account, chainId, active } = useWeb3React();
   const showConnectAWallet = Boolean(!account);
   const context = useWeb3React();
   const { library } = context;
-const [isended, setisended] = useState(false)
-const videoRef: any = useRef(null);
-useEffect(() => {
-  videoRef.current.defaultMuted = true;
-})
+  const [isended, setisended] = useState(false);
+  const videoRef: any = useRef(null);
+  const audioRef: any = useRef(null);
 
-const attemptPlay = () => {
-  videoRef &&
-  videoRef.current &&
-  videoRef.current.load() &&
-  videoRef.current.play().catch((error: any) => {console.log("error attempting to play", error)})
-}
-useEffect(() => {
-attemptPlay()
-},[])
-  function RenderButtons(){
-    setisended(true)
+  useEffect(() => {
+    videoRef.current.defaultMuted = true;
+  });
+
+  const attemptPlay = () => {
+    videoRef &&
+      videoRef.current &&
+      videoRef.current.load() &&
+      videoRef.current.play().catch((error: any) => {
+        console.log("error attempting to play", error);
+      });
+  };
+
+  useEffect(() => {
+    attemptPlay();
+  }, []);
+
+  function RenderButtons() {
+    setisended(true);
   }
- // dangerouslySetInnerHTML={{
- //   __html: `<video className="app__backgroundVideo" autoplay loop muted playsinline>
-//<source src='./newvideo.mp4' type="video/mp4" />
-//Your browser does not support the video tag.
-//</video>`,
-  //}}
-
 
   return (
     <>
+      <div className="h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
+        <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+          <video
+            ref={videoRef}
+            className="min-w-full min-h-full absolute object-cover"
+            playsInline
+            onEnded={() => RenderButtons()}
+            autoPlay
+            muted
+          >
+            <source src="./FinalRender.mp4" type="video/mp4" /> Your browser
+            does not support the video tag, update your browser
+          </video>
+        </div>
 
-<div className=" h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
-  
-    <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
-        <video ref={videoRef} className="min-w-full min-h-full absolute object-cover" playsInline onEnded={()=>RenderButtons()} autoPlay muted >
-        <source src="./FinalRender.mp4" type='video/mp4'/> Your browser does not support the video tag, update your browser
-        </video>
-
-    </div>
-    <DappComponent ended={isended}></DappComponent>
-</div>
-
-
-    
-
-
-
-        </>
+        <DappComponent ended={isended}></DappComponent>
+      </div>
+    </>
   );
 };
 
